1bc8e04073adbf31e6cf3c166f210016a3b4393e
zequihg50 <zequi3000@hotmail.com>
2019-06-02 11:14:58 +0200

    Added synchronous vs threads in local test case

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TFM
 
-## Local
+## Local (1 file)
 
 Create your environment:
 
@@ -8,8 +8,19 @@ Create your environment:
 name=NAME
 conda create -n $name -c conda-forge xarray zarr jupyter matplotlib dask nco numcodecs bokeh
 conda activate $name
-#pip install h5pyd
 ./local.sh
 ```
 
 Run jupyter notebook and run `local/Local.ipynb`.
+
+## Local (full dataset)
+
+```bash
+find $(pwd)/original -type f | parallel nccopy -d 0 -4 -c "time/2739,lat/8,lon/32" {} chunked/{/}
+```
+
+## Remote
+
+```bash
+pip install h5pyd
+```
--- netcdf-to-zarr.py
+++ netcdf-to-zarr.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
-import time
+import sys,time
 import dask, xarray, zarr
 
 zarr.storage.default_compressor = None
 
-i = 'shared/original/tas_AERhr_CNRM-ESM2-1_historical_r1i1p1f2_gr_185001010030-185412312330.nc'
-o = 'shared/zarr/tas_AERhr_CNRM-ESM2-1_historical_r1i1p1f2_gr_185001010030-185412312330'
+i = sys.argv[1]
+o = sys.argv[2]
 
-start = time.time()
 ds = xarray.open_dataset(i, engine='netcdf4')
-end1 = time.time()
-
 # this will create a zarr array with the following chunking: {'time': 2739, 'lat': 8, 'lon': 32}
 ds.to_zarr(o)
-end2 = time.time()
-
-print('Time to open: {}. Time to save: {}.'.format(end1 - start, end2 - end1))
